faa0f7494e1b455d1b878aae6272f0c8df3a425b
amandasatya <amandasatya93@gmail.com>
2024-08-18 19:09:48 +0700

    fix porto

--- src/components/portofolio/index.tsx
+++ src/components/portofolio/index.tsx
@@ -28,11 +28,11 @@ const WorksPage = () => {
           <h1 className="text-3xl font-bold text-sky-600 flex justify-center items-center p-5">
             My Portofolio
           </h1>
-          <div className="xl:grid xl:grid-cols-3 gap-2 xl:px-40">
+          <div className="lg:grid lg:grid-cols-3 gap-2 xl:px-40">
             {currentProjects.map((project, index) =>
               project.source_code_link ? (
                 <Link
-                  className="xl:p-1 bg-slate-700 rounded-xl shadow-inner shadow-black "
+                  className="xl:p-1  rounded-xl shadow-inner shadow-black "
                   key={index}
                   href={project.source_code_link}
                 >
@@ -41,36 +41,38 @@ const WorksPage = () => {
                     alt={project.name || "Project Image"}
                     className="xl:rounded-t-xl object-cover"
                   />
-                  <div className="p-5">
-                    <label
-                      htmlFor=""
-                      className="text-center flex justify-center items-center text-xl font-bold"
-                    >
-                      {project.name}
-                    </label>
-                    <h1 className="text-center">{project.description}</h1>
-                  </div>
-                  <div className="p-2">
-                    <label
-                      htmlFor=""
-                      className="text-center flex justify-center items-center"
-                    >
-                      <div key={index} className="flex gap-1">
-                        {project?.tags?.map((tag, tagIndex) => (
-                          <div
-                            key={tagIndex}
-                            className="flex flex-wrap gap-1 justify-center items-center text-center"
-                          >
-                            <img
-                              src={tag.icon?.src}
-                              alt=""
-                              className="flex h-6 w-6"
-                            />
-                            <h1>{tag.name}</h1>{" "}
-                          </div>
-                        ))}
-                      </div>
-                    </label>
+                  <div className="bg-slate-700 sm:h-56 lg:h-56 rounded-b-lg">
+                    <div className="p-5">
+                      <label
+                        htmlFor=""
+                        className="text-center flex justify-center items-center text-xl font-bold"
+                      >
+                        {project.name}
+                      </label>
+                      <h1 className="text-center">{project.description}</h1>
+                    </div>
+                    <div className="p-2">
+                      <label
+                        htmlFor=""
+                        className="text-center flex justify-center items-center"
+                      >
+                        <div key={index} className="flex gap-1">
+                          {project?.tags?.map((tag, tagIndex) => (
+                            <div
+                              key={tagIndex}
+                              className="flex flex-wrap gap-1 justify-center items-center text-center"
+                            >
+                              <img
+                                src={tag.icon?.src}
+                                alt=""
+                                className="flex h-6 w-6"
+                              />
+                              <h1>{tag.name}</h1>{" "}
+                            </div>
+                          ))}
+                        </div>
+                      </label>
+                    </div>
                   </div>
                 </Link>
               ) : null
